5797a1597d58e1fb87fadbac3cf282020bd6c307
Otavio Silva <otavioolsilva@gmail.com>
2020-05-05 12:42:50 -0300

    Relatorio gerado

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main()
 {
     string sexo;
     int qCerveja = 0, qRefri = 0, qEspetos = 0;
+    double consumo, couvert, ingresso, total;
+
+    //Leitura dos dados
 
     cout << "Sexo: ";
     cin >> sexo;
@@ -13,8 +16,34 @@ int main()
     cin >> qCerveja;
     cout << "Quantidade de refrigerantes: ";
     cin >> qRefri;
-    cout << "Quantidade de espetos: ";
+    cout << "Quantidade de espetinhos: ";
     cin >> qEspetos;
+
+    //Calculos
+
+    if(sexo == "F"){
+        ingresso = 8.00;
+    } else {
+        ingresso = 10.00;
+    }
+
+    consumo = (qCerveja * 5.0) + (qRefri * 3.0) + (qEspetos * 7.0);
+
+    if(consumo > 30.0) couvert = 0.0;
+    else couvert = 4.0;
+
+    total = consumo + couvert + ingresso;
+
+    //Relatorios
+
+    cout << fixed << setprecision(2);
+
+    cout << endl << "RELATORIO:" << endl;
+    cout << "Consumo = R$ " << consumo << endl;
+    if(couvert == 0.0) cout << "Isento de Couvert" << endl;
+    else cout << "Couvert = R$ " << couvert << endl;
+    cout << "Ingresso = R$ " << ingresso << endl;
+    cout << endl << "Valor a pagar = R$ " << total << endl;
 
     return 0;
 }
