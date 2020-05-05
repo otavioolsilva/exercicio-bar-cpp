579f79d4c494ca4c9734655900d6cc608ce5140f
Otavio Silva <otavioolsilva@gmail.com>
2020-05-05 12:25:12 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string sexo;
+    int qCerveja = 0, qRefri = 0, qEspetos = 0;
+
+    cout << "Sexo: ";
+    cin >> sexo;
+    cout << "Quantidade de cervejas: ";
+    cin >> qCerveja;
+    cout << "Quantidade de refrigerantes: ";
+    cin >> qRefri;
+    cout << "Quantidade de espetos: ";
+    cin >> qEspetos;
+
     return 0;
 }
